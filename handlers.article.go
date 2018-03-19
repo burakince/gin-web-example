fe63f7d6b7648667e4787fde2c8d2d2dfdb73a36
Burak Ince <bince@thoughtworks.com>
2018-03-20 00:04:26 +0300

    Add article detail pages

--- handlers.article.go
+++ handlers.article.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "net/http"
+  "strconv"
 
   "github.com/gin-gonic/gin"
 )
@@ -12,8 +13,27 @@ func showIndexPage(c *gin.Context) {
     http.StatusOK,
     "index.html",
     gin.H{
-      "title":   "Home Page",
+      "title": "Home Page",
       "payload": articles,
     },
   )
+}
+
+func getArticle(c *gin.Context) {
+  if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
+    if article, err := getArticleByID(articleID); err == nil {
+      c.HTML(
+        http.StatusOK,
+        "article.html",
+        gin.H{
+          "title":   article.Title,
+          "payload": article,
+        },
+      )
+    } else {
+      c.AbortWithError(http.StatusNotFound, err)
+    }
+  } else {
+    c.AbortWithStatus(http.StatusNotFound)
+  }
 }
