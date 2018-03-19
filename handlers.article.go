064f1a32163cbc81d079fdf78878b8143487cf30
Burak Ince <bince@thoughtworks.com>
2018-03-20 01:01:11 +0300

    Add json and xml response functionality

--- handlers.article.go
+++ handlers.article.go
@@ -1,39 +1,33 @@
 package main
 
 import (
-  "net/http"
-  "strconv"
+	"net/http"
+	"strconv"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func showIndexPage(c *gin.Context) {
-  articles := getAllArticles()
-  c.HTML(
-    http.StatusOK,
-    "index.html",
-    gin.H{
-      "title": "Home Page",
-      "payload": articles,
-    },
-  )
+	articles := getAllArticles()
+	data := gin.H{
+		"title":   "Home Page",
+		"payload": articles,
+	}
+	render(c, data, "index.html")
 }
 
 func getArticle(c *gin.Context) {
-  if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
-    if article, err := getArticleByID(articleID); err == nil {
-      c.HTML(
-        http.StatusOK,
-        "article.html",
-        gin.H{
-          "title":   article.Title,
-          "payload": article,
-        },
-      )
-    } else {
-      c.AbortWithError(http.StatusNotFound, err)
-    }
-  } else {
-    c.AbortWithStatus(http.StatusNotFound)
-  }
+	if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
+		if article, err := getArticleByID(articleID); err == nil {
+			data := gin.H{
+				"title":   article.Title,
+				"payload": article,
+			}
+			render(c, data, "article.html")
+		} else {
+			c.AbortWithError(http.StatusNotFound, err)
+		}
+	} else {
+		c.AbortWithStatus(http.StatusNotFound)
+	}
 }
--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 var router *gin.Engine
 
 func main() {
-  router = gin.Default()
-  router.LoadHTMLGlob("templates/*")
+	router = gin.Default()
+	router.LoadHTMLGlob("templates/*")
 
-  initializeRoutes()
+	initializeRoutes()
 
-  router.Run()
+	router.Run()
+}
+
+func render(c *gin.Context, data gin.H, templateName string) {
+	switch c.Request.Header.Get("Accept") {
+	case "application/json":
+		c.JSON(http.StatusOK, data["payload"])
+	case "application/xml":
+		c.XML(http.StatusOK, data["payload"])
+	default:
+		c.HTML(http.StatusOK, templateName, data)
+	}
 }
